2fcd0acdb37f49fe3db7278ce9649b90fb1e67b4
hitska <gobbler.kun@gmail.com>
2019-01-09 02:02:48 +0400

    Переделано из-за обновления сайта
    Предыдущая версия перестала работать вместе с обновлением сосача. Заодно выкинул весь говнокод, заменив на jQuery.

--- rf-trips.user.js
+++ rf-trips.user.js
@@ -1,51 +1,13 @@
 // ==UserScript==
 // @name        rf-trips
 // @description Добавляет поле имени в /rf/
-// @version     1.2
+// @version     1.3
 // @include     *2ch.*/rf/*
 // @grant       none
-// @author      Василий Лисец
+// @author      Твоя мамаша
+// @require		https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
-// какой-то говнокод...
-function findChildTagByName(obj, name) {
-	if (obj == null)
-		return null;
-	
-	var childs = obj.childNodes;
-	for(var i = 0; i < childs.length; i++)
-		if (childs[i].tagName == name)
-			return childs[i];
-
-	return null;
-}
-
-function findInTableByClass(obj, name) {
-	if (obj == null)
-		return null;
-	
-	var childs = obj.childNodes;
-	for(var i = 0; i < childs.length; i++)
-		if (childs[i].className == name)
-			return childs[i];
-
-	return null;
-}
-
 (function (window, undefined) {
-	// Спускаемся от формы постинга до места, куда будем вставлять имя
-	var form = document.getElementById("postform");
-	var table = findChildTagByName(form, "TABLE");
-	var tbody = findChildTagByName(table, "TBODY");
-  if (tbody == null)	// Скрипт устарел
-    return;
-	var nameTR = findInTableByClass(tbody, "name");
-	if (nameTR != null) // Имена уже разрешены.
-		return;
-		
-	var subjectTR = findInTableByClass(tbody, "post-subject");	
-	nameTR = document.createElement('tr');
-	nameTR.className = "name";
-	nameTR.innerHTML = '<td class="label desktop"><label for="name">Трип</label></td><td><input type="text" value="" id="name" name="name" size="30" placeholder="заполняя это поле, знай что я тебя ненавижу"></td>';
-	tbody.insertBefore(nameTR, subjectTR);
+	$('#e-mail').after('<input type="text" value="" id="name" class="postform__input postform__input_type_m input" size="10" name="name" placeholder="трипкод">');
 })(window);
